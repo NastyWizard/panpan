0e969b16a8107a5c7710655d6601b33448d2bd04
NastyWizard <detrocasey@gmail.com>
2026-05-22 14:33:56 -0400

    Added helpers to rect struct for getting non-overlapping rects and equality

--- util/Math.cs
+++ util/Math.cs
@@ -22,7 +22,7 @@ namespace panpan.Util
     }
 
     [StructLayout(LayoutKind.Sequential)]
-    public struct Rect
+    public struct Rect : IEquatable<Rect>
     {
         public int X, Y, Width, Height, OriginX, OriginY;
         public float Angle;
@@ -32,6 +32,18 @@ namespace panpan.Util
             Y = y;
             Width = w;
             Height = h;
+
+            OriginX = 0;
+            OriginY = 0;
+            Angle = 0;
+        }
+
+        public Rect(Rect other)
+        {
+            X = other.X;
+            Y = other.Y;
+            Width = other.Width;
+            Height = other.Height;
 
             OriginX = 0;
             OriginY = 0;
@@ -114,6 +126,121 @@ namespace panpan.Util
         public override string ToString()
         {
             return $"[{X}, {Y}, {Width}, {Height}, ox:{OriginX}, oy:{OriginY}]";
+        }
+        
+        public override bool Equals(object? obj)
+        {
+            return obj is Rect other && Equals(other);
+        }
+        public bool Equals(Rect other)
+        {
+            return X == other.X &&
+                Y == other.Y &&
+                Width == other.Width &&
+                Height == other.Height &&
+                OriginX == other.OriginX &&
+                OriginY == other.OriginY &&
+                Angle == other.Angle;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(
+                X, Y,
+                Width, Height,
+                OriginX, OriginY,
+                Angle
+            );
+        }
+
+        public static bool operator ==(Rect left, Rect right)
+        {
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(Rect left, Rect right)
+        {
+            return !left.Equals(right);
+        }
+    }
+
+    public static class RectUtil
+    {
+        public static List<Rect> Subtract(Rect a, Rect b)
+        {
+            List<Rect> result = new();
+
+            int ix = Math.Max(a.X, b.X);
+            int iy = Math.Max(a.Y, b.Y);
+
+            int ix2 = Math.Min(a.X + a.Width,  b.X + b.Width);
+            int iy2 = Math.Min(a.Y + a.Height, b.Y + b.Height);
+
+            int iw = ix2 - ix;
+            int ih = iy2 - iy;
+
+            // no overlap
+            if (iw <= 0 || ih <= 0)
+            {
+                result.Add(a);
+                return result;
+            }
+
+            // top
+            if (iy > a.Y)
+            {
+                result.Add(new Rect(
+                    a.X,
+                    a.Y,
+                    a.Width,
+                    iy - a.Y
+                ));
+            }
+
+            // bottom
+            if (iy2 < a.Y + a.Height)
+            {
+                result.Add(new Rect(
+                    a.X,
+                    iy2,
+                    a.Width,
+                    (a.Y + a.Height) - iy2
+                ));
+            }
+
+            // left
+            if (ix > a.X)
+            {
+                result.Add(new Rect(
+                    a.X,
+                    iy,
+                    ix - a.X,
+                    ih
+                ));
+            }
+
+            // right
+            if (ix2 < a.X + a.Width)
+            {
+                result.Add(new Rect(
+                    ix2,
+                    iy,
+                    (a.X + a.Width) - ix2,
+                    ih
+                ));
+            }
+
+            return result;
+        }
+
+        public static List<Rect[]> NonOverlapping(Rect a, Rect b)
+        {
+            List<Rect[]> result = new();
+
+            result.Add(Subtract(a, b).ToArray());
+            result.Add(Subtract(b, a).ToArray());
+
+            return result;
         }
     }
 }
